91f3471037070155a5f2910d8efbbcba6587e5b5
David Crane <davidc@donorschoose.org>
2016-10-24 16:08:09 -0400

    Lock in explicit pg-0.18.4.gem version

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -54,7 +54,9 @@ rescue LoadError
   end
 
   begin
-    chef_gem "pg"
+    chef_gem "pg" do
+      version '0.18.4'
+    end
   rescue Gem::Installer::ExtensionBuildError => e
     # Are we an omnibus install?
     raise if RbConfig.ruby.scan(%r{(chef|opscode)}).empty?
